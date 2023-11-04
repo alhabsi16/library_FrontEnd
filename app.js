e48e91cd6d80732ee47a427578944904ec1c38a4
alhabsi16 <mxmx8283@gmail.com>
2023-11-05 00:00:00 +0400

    frontEnd

---
+++ app.js
@@ -0,0 +1,38 @@
+const apiUrl = 'https://localhost:7041/api/controller/byPublicationYear/2011'
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+showMovies(apiUrl);
+function showMovies(url){
+    fetch(url).then(res => res.json())
+    .then(function(data){
+    console.log(data.results);
+    data.results.forEach(element => {
+        const el = document.createElement('div');
+        const image = document.createElement('img');
+        const text = document.createElement('h2');
+
+        text.innerHTML = `${element.title}`;
+        image.src = IMGPATH + element.poster_path;
+        el.appendChild(image);
+        el.appendChild(text);
+        main.appendChild(el);
+    }); 
+});
+}
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+     
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        showMovies(SEARCHAPI + searchTerm);
+        search.value = "";
+    }
+});
+
